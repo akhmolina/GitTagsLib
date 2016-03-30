71a271387385bec1c5b7ffc5922fcdc5f50bf165
Valentina Akhmolina <valentina.ahmolina@yandex.ru>
2016-03-30 21:36:14 +0300

    Add repository tags to xml converter

---
+++ GitTagsLib/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml;
+using LibGit2Sharp;
+
+namespace GitTagsLib
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string dataFormat = "r";
+            FolderBrowserDialog fileDialog = new FolderBrowserDialog();
+
+            DialogResult result = fileDialog.ShowDialog();
+            if (result != DialogResult.OK)
+            { MessageBox.Show("Папка не выбрана."); Environment.Exit(0); }
+
+            string repoPath = fileDialog.SelectedPath;
+            Repository repo = null;
+
+            try {repo = new Repository(repoPath);}
+            catch (Exception ex)
+            { MessageBox.Show("Не удалось создать репозиторий.\n" + ex.Message); Environment.Exit(0); }
+
+            using (repo)
+            {
+                var tags = repo.Tags.Select(
+                    t => new { name = t.FriendlyName.ToString(),
+                            date = ((Commit)t.Target).Author.When.ToString(dataFormat, CultureInfo.InvariantCulture) });
+                if (tags.Count() > 0)
+                {
+                    XmlDocument tagsXML = new XmlDocument();
+                    XmlNode root = tagsXML.CreateNode(XmlNodeType.Element, "root", "");
+                    tagsXML.AppendChild(root);
+
+                    foreach (var tag in tags)
+                    {
+                        XmlNode xmltag = tagsXML.CreateNode(XmlNodeType.Element, "tag", "");
+
+                        XmlAttribute nameAttribute = tagsXML.CreateAttribute("name");
+                        nameAttribute.Value = tag.name;
+                        xmltag.Attributes.Append(nameAttribute);
+
+                        XmlAttribute dateAttribute = tagsXML.CreateAttribute("date");
+                        dateAttribute.Value = tag.date;
+                        xmltag.Attributes.Append(dateAttribute);
+
+                        root.AppendChild(xmltag);
+                    }
+                    try
+                    {
+                        tagsXML.Save(repoPath + @"\..\tags.xml");
+                    }
+                    catch (Exception ex)
+                    { MessageBox.Show("Не удалось сохранить файл xml.\n" + ex.Message); }
+                }
+                else { MessageBox.Show("В репозитории нет тегов."); }
+            }
+        }
+    }
+}
---
+++ GitTagsLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("GitTagsLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GitTagsLib")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b86699bb-c861-49d2-bd91-8f6f5de5d55b")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
